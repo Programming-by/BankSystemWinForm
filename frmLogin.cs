5de070e43bac433dc5dd5db52648815d68ca2b4b
AnasNaamneh <naamnih.anas@gmail.com>
2023-09-26 00:46:43 -0700

    Refactor Code at Login Form

--- frmLogin.cs
+++ frmLogin.cs
@@ -8,7 +8,7 @@ namespace BankSystemWinForm
     public partial class frmLogin : Form
     {
         clsUser _User;
-        clsUser _UserFind;
+        clsUser _LoggedInUser;
         string _PinCode;
         int Counter = 3;
         clsLoginRegister _LoginRegister;
@@ -47,35 +47,38 @@ namespace BankSystemWinForm
             {
                 if (txtUserName.Text == _User.UserName && txtPinCode.Text == _User.PinCode)
                 {
-                    _LoginRegister = new clsLoginRegister();
 
-                    _UserFind = clsUser.Find(_User.UserName);
+                    LoginRegister();
+
                     _PinCode = _User.PinCode;
 
-                    _LoginRegister.Date = DateTime.Now;
-                    _LoginRegister.UserName = _User.UserName;
-                    _LoginRegister.Password = _User.PinCode;
-                    _LoginRegister.Permissions = _UserFind.Permissions;
-
-
-                    if (_LoginRegister.Save())
-                    {
-                        Console.WriteLine("Saved Record");
-                    }
-
-
-                    frmListClients frm = new frmListClients(_UserFind,_PinCode);
+                    frmListClients frm = new frmListClients(_LoggedInUser,_PinCode);
 
                     frm.DataBack += frmListClients_DataBack;
 
                     frm.ShowDialog();
-
-                    
-
                 }
             }
         }
 
+
+        private void LoginRegister()
+        {
+            _LoginRegister = new clsLoginRegister();
+
+            _LoggedInUser = clsUser.Find(_User.UserName);
+
+            _LoginRegister.Date = DateTime.Now;
+            _LoginRegister.UserName = _User.UserName;
+            _LoginRegister.Password = _User.PinCode;
+            _LoginRegister.Permissions = _LoggedInUser.Permissions;
+
+
+            if (_LoginRegister.Save())
+            {
+                Console.WriteLine("Saved Record");
+            }
+        }
         public void frmListClients_DataBack(string UserName , string PinCode)
         {
             txtUserName.Text = UserName;
